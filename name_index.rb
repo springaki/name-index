fe2dc338a1f7e148dd02dae2a6c6b34af1ea4e42
Aki <aki.onetime@gmail.com>
2014-05-30 20:58:59 +0900

    refactoring

--- name_index.rb
+++ name_index.rb
@@ -1,30 +1,31 @@
 class NameIndex
-  CATEGORIZE_RULES = [
-      {:rule => /^[ァ-オヴ]/, :category => "ア"},
-      {:rule => /^[カ-ゴヵヶ]/, :category => "カ"},
-      {:rule => /^[サ-ゾ]/, :category => "サ"},
-      {:rule => /^[タ-ド]/, :category => "タ"},
-      {:rule => /^[ナ-ノ]/, :category => "ナ"},
-      {:rule => /^[ハ-ポ]/, :category => "ハ"},
-      {:rule => /^[マ-モ]/, :category => "マ"},
-      {:rule => /^[ャ-ヨ]/, :category => "ヤ"},
-      {:rule => /^[ラ-ロ]/, :category => "ラ"},
-      {:rule => /^[ヮ-ヲヷヸヹヺ]/, :category => "ワ"},
-      {:rule => /^[ン]/, :category => "ン"},
+  GROUPING_RULES = [
+      {:rule => /^[ァ-オヴ]/, :group => "ア"},
+      {:rule => /^[カ-ゴヵヶ]/, :group => "カ"},
+      {:rule => /^[サ-ゾ]/, :group => "サ"},
+      {:rule => /^[タ-ド]/, :group => "タ"},
+      {:rule => /^[ナ-ノ]/, :group => "ナ"},
+      {:rule => /^[ハ-ポ]/, :group => "ハ"},
+      {:rule => /^[マ-モ]/, :group => "マ"},
+      {:rule => /^[ャ-ヨ]/, :group => "ヤ"},
+      {:rule => /^[ラ-ロ]/, :group => "ラ"},
+      {:rule => /^[ヮ-ヲヷヸヹヺ]/, :group => "ワ"},
+      {:rule => /^[ン]/, :group => "ン"},
   ].freeze
 
-  def self.create_index(names)
-    names.sort.chunk{|item|
-      grouping(item)
-    }.each_with_object([]) {|item, categorize_names|
-      categorize_names << item
-    }
-  end
+  class << self
+    def create_index(names)
+      names.sort.chunk{|item|
+        find_group(item)
+      }.each_with_object([]) {|item, grouped_names|
+        grouped_names << item
+      }
+    end
 
-  def self.grouping(name)
-
-    CATEGORIZE_RULES.each { |rule|
-      return rule[:category] if name =~ rule[:rule]
-    }
+    def find_group(name)
+      GROUPING_RULES.each { |rule|
+        return rule[:group] if name =~ rule[:rule]
+      }
+    end
   end
 end
