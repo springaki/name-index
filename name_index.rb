997297d682d2d0ff548c815abff33e1151bdd43c
Aki <aki.onetime@gmail.com>
2014-05-30 20:36:47 +0900

    最小構成の実装

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,14 @@
 class NameIndex
   def self.create_index(names)
-    []
+    names.sort.chunk{|item|
+      grouping(item)
+    }.each_with_object([]) {|item, categorize_names|
+      categorize_names << item
+    }
+  end
+
+  def self.grouping(name)
+    return "ア" if name=~/^[ァ-オ]/
+    return "カ" if name=~/^[カ-ゴ]/
   end
 end
--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -2,4 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
   specify { expect(NameIndex.create_index([])).to eq [] }
+  specify { expect(NameIndex.create_index(['キシモト', 'イトウ', 'カネダ'])).to eq [ ['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']] ] }
+
+  # 段の中の並び
+  specify {expect(NameIndex.create_index(['キシモト', 'イトウ', 'カネダ', 'アイザワ','エトウ','オザキ','ウドウ',])).to eq [ ['ア', ['アイザワ', 'イトウ', 'ウドウ', 'エトウ', 'オザキ']], ['カ', ['カネダ', 'キシモト']] ] }
+
 end
